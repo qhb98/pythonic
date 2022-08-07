f47d6e12decc3f699a4a09c82e74baa11a1ac4c6
qhb98 <2651311819@qq.com>
2022-08-07 20:16:21 +0800

    init

---
+++ python代码规范/pythonic.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+# @FileName: :pythonic.py
+# @Time: 2022/8/7 20:11
+# @Author: QHB
+
+
+"""
+1. 干净的python代码是很重要的
+2. 编写更好的注释、文档字符串和注解
+3. python中的命名和代码布局
+4. python中的控制结构
+5. 整洁的python代码模式
+6. 代码质量执行工具
+"""
+
+
+
+
+
